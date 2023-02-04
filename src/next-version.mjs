918dba17943617c307a8e2ee5ee5043118898f35
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-04 11:06:38 -0600

    lint fixes

--- src/next-version.mjs
+++ src/next-version.mjs
@@ -1,19 +1,19 @@
 import createError from 'http-errors'
 import semver from 'semver'
 
-const STYLE_SEMVER='semver'
-const STYLE_TIMEVER='timever'
-const STYLE_AUTO='auto'
+const STYLE_SEMVER = 'semver'
+const STYLE_TIMEVER = 'timever'
+const STYLE_AUTO = 'auto'
 
-const validIncrements = [ 'major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease', 'pretype' ]
+const validIncrements = ['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease', 'pretype']
 
 const makeTS = ({ date = new Date() } = {}) => {
-  const timestamp = date.getUTCFullYear() 
-    + (date.getUTCMonth() + 1 + '').padStart(2, '0') 
+  const timestamp = date.getUTCFullYear()
+    + (date.getUTCMonth() + 1 + '').padStart(2, '0')
     + (date.getUTCDate() + '').padStart(2, '0')
-    + '.' 
-    + (date.getUTCHours() + '').padStart(2, '0') 
-    + (date.getUTCMinutes() + '').padStart(2, '0') 
+    + '.'
+    + (date.getUTCHours() + '').padStart(2, '0')
+    + (date.getUTCMinutes() + '').padStart(2, '0')
     + (date.getUTCSeconds() + '').padStart(2, '0')
     + 'Z'
 
@@ -21,27 +21,26 @@ const makeTS = ({ date = new Date() } = {}) => {
 }
 
 /**
- * Given a current version, increment, and optional style, generates the next version string. The version style is 
- * typically identified automatically but can be specified explicitly (if switching styles, for example). Supports 
+ * Given a current version, increment, and optional style, generates the next version string. The version style is
+ * typically identified automatically but can be specified explicitly (if switching styles, for example). Supports
  * 'semver' and 'timever' style. See [semver specification](https://semver.org/). Timever style consists of:
  * - a major version integer (indicating compatibility series)
  * - a UTC timestamp in the form of 'YYYYMMDD.HHMMSSZ'
  * - the timestamp is generated during the execution of this method.
- * 
+ *
  * For semver style, this method follows the [semver spec](https://semver.org) except that:
  * - `increment` 'prerelease' cannot be used with non-prerelease versions (results in execption)
  * - supports 'pretype' and specific pre-release sequence 'alpha' -> 'beta' -> 'rc' -> released
- * 
+ *
  * #### Parameters
  * - `currVer`: the current version to increment.
- * - `increment`: what to inrement; may be: 'major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 
- *   'prerelease', or 'pretype'. The first seven are defined by the [semver spec](https://semver.org) and 'pretype' 
- *    means to increment the pre-release ID from 'alpha' -> 'beta' -> 'rc' -> none. Passing in a `currVer` with any 
+ * - `increment`: what to inrement; may be: 'major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch',
+ *   'prerelease', or 'pretype'. The first seven are defined by the [semver spec](https://semver.org) and 'pretype'
+ *    means to increment the pre-release ID from 'alpha' -> 'beta' -> 'rc' -> none. Passing in a `currVer` with any
  *    other prerelease ID with a 'pretype' increment will result in undefined results
  */
-const nextVersion = ({ currVer, date, increment, style=STYLE_AUTO }) => {
-  if (increment !== undefined && !validIncrements.includes(increment))
-    throw createError.BadRequest(`Invalid increment '${increment}' specified.`)
+const nextVersion = ({ currVer, date, increment, style = STYLE_AUTO }) => {
+  if (increment !== undefined && !validIncrements.includes(increment)) { throw createError.BadRequest(`Invalid increment '${increment}' specified.`) }
   // what are we incrementing? default is patch for released and prerelease for pre-release projects
   increment = increment || (currVer.match(/^[\d.Z-]+$/) ? 'patch' : 'prerelease')
 
@@ -50,10 +49,8 @@ const nextVersion = ({ currVer, date, increment, style=STYLE_AUTO }) => {
   let currType = currVer.replace(/^[\d.Z-]+(alpha|beta|rc)(?:\.\d+)?/, '$1')
   if (currType === currVer) currType = null
 
-  if (currType === null && increment === 'prerelease')
-    throw createError.BadRequest('Cannot increment prerelease for non-prerelease versions.')
-  if (currType !== null && !(increment === 'prerelease' || increment === 'pretype'))
-    throw createError.BadRequest(`Prerelease version ${currVer} can only be incremented by 'prerelease' or 'pretype'.`)
+  if (currType === null && increment === 'prerelease') { throw createError.BadRequest('Cannot increment prerelease for non-prerelease versions.') }
+  if (currType !== null && !(increment === 'prerelease' || increment === 'pretype')) { throw createError.BadRequest(`Prerelease version ${currVer} can only be incremented by 'prerelease' or 'pretype'.`) }
 
   if (style === STYLE_AUTO || style === undefined) {
     style = currVer.match(/\d{6}Z(?:-(?:alpha|beta|rc)\.\d+)?$/) ? STYLE_TIMEVER : STYLE_SEMVER
@@ -84,13 +81,13 @@ const nextVersion = ({ currVer, date, increment, style=STYLE_AUTO }) => {
   else { // it's 'timever' style
     const timestamp = makeTS({ date })
 
-    const [ currMajor ] = currVer.split('.')
+    const [currMajor] = currVer.split('.')
     const nextMajor = increment === 'major' || increment === 'premajor' ? '' + (parseInt(currMajor) + 1) : currMajor
     if (currType === null && increment.startsWith('pre')) {
       nextVer = nextMajor + '.' + timestamp + '-alpha.0'
     }
     else if (currType !== null && increment === 'prerelease') {
-      const currPreReleaseString = (currVer.match(/(?:alpha|beta|rc)\.(\d+)$/) || [ null, null ])[1]
+      const currPreReleaseString = (currVer.match(/(?:alpha|beta|rc)\.(\d+)$/) || [null, null])[1]
       const currPreRelease = parseInt(currPreReleaseString)
       const currBare = currVer.replace(/\d+$/, '')
       nextVer = currBare + (currPreRelease + 1)
--- src/test/next-version.test.js
+++ src/test/next-version.test.js
@@ -13,100 +13,100 @@ describe('makeTS', () => {
 describe('nextVersion', () => {
   describe('handles semver style', () => {
     test.each([
-      [ '1.0.0', undefined, '1.0.1' ],
-      [ '1.0.0', 'patch', '1.0.1' ],
-      [ '1.0.0', 'minor', '1.1.0' ],
-      [ '1.0.0', 'major', '2.0.0' ],
-      [ '1.0.0', 'prepatch', '1.0.1-alpha.0' ],
-      [ '1.0.0', 'preminor', '1.1.0-alpha.0' ],
-      [ '1.0.0', 'premajor', '2.0.0-alpha.0' ],
-      [ '1.0.0-alpha.1', 'pretype', '1.0.0-beta.0' ],
-      [ '1.0.0-beta.1', 'pretype', '1.0.0-rc.0' ],
-      [ '1.0.0-rc.1', 'pretype', '1.0.0' ]
+      ['1.0.0', undefined, '1.0.1'],
+      ['1.0.0', 'patch', '1.0.1'],
+      ['1.0.0', 'minor', '1.1.0'],
+      ['1.0.0', 'major', '2.0.0'],
+      ['1.0.0', 'prepatch', '1.0.1-alpha.0'],
+      ['1.0.0', 'preminor', '1.1.0-alpha.0'],
+      ['1.0.0', 'premajor', '2.0.0-alpha.0'],
+      ['1.0.0-alpha.1', 'pretype', '1.0.0-beta.0'],
+      ['1.0.0-beta.1', 'pretype', '1.0.0-rc.0'],
+      ['1.0.0-rc.1', 'pretype', '1.0.0']
     ])("%s + '%s' -> %s", (currVer, increment, expected) => expect(ver.nextVersion({ currVer, increment })).toBe(expected))
 
     test.each([
-      [ '1.0.0', 'prerelease' ],
-      [ '1.0.0-foo.1', 'pretype' ],
-      [ '1.0.0', 'primary' ],
-      [ '1.0.0-alpha.1', 'major' ],
-      [ '1.0.0-alpha.1', 'premajor' ],
-      [ '1.0.0-alpha.1', 'minor' ],
-      [ '1.0.0-alpha.1', 'preminor' ],
-      [ '1.0.0-alpha.1', 'patch' ],
-      [ '1.0.0-alpha.1', 'prepatch' ],
-    ])("%s + %s -> throws an exception", (currVer, increment) => 
-      expect(() => ver.nextVersion({ currVer, increment})).toThrow())
+      ['1.0.0', 'prerelease'],
+      ['1.0.0-foo.1', 'pretype'],
+      ['1.0.0', 'primary'],
+      ['1.0.0-alpha.1', 'major'],
+      ['1.0.0-alpha.1', 'premajor'],
+      ['1.0.0-alpha.1', 'minor'],
+      ['1.0.0-alpha.1', 'preminor'],
+      ['1.0.0-alpha.1', 'patch'],
+      ['1.0.0-alpha.1', 'prepatch']
+    ])('%s + %s -> throws an exception', (currVer, increment) =>
+      expect(() => ver.nextVersion({ currVer, increment })).toThrow())
   })
 
   describe('handles timever style', () => {
     let preTS, resultReleased, resultPreInit, resultPreOngoing, resultPretype, postTS
     const startTimeVer = '1.20230103.051325Z'
     const testReleased = [
-      [ startTimeVer, undefined, '1' ],
-      [ startTimeVer, 'patch', '1' ],
-      [ startTimeVer, 'minor', '1' ],
-      [ startTimeVer, 'major', '2' ]
+      [startTimeVer, undefined, '1'],
+      [startTimeVer, 'patch', '1'],
+      [startTimeVer, 'minor', '1'],
+      [startTimeVer, 'major', '2']
     ]
     const testPreInit = [
-      [ startTimeVer, 'prepatch', '1', '-alpha.0' ],
-      [ startTimeVer, 'preminor', '1', '-alpha.0' ],
-      [ startTimeVer, 'premajor', '2', '-alpha.0' ]
+      [startTimeVer, 'prepatch', '1', '-alpha.0'],
+      [startTimeVer, 'preminor', '1', '-alpha.0'],
+      [startTimeVer, 'premajor', '2', '-alpha.0']
     ]
     const testPreOngoing = [
-      [ startTimeVer + '-alpha.1', 'prerelease', '-alpha.2' ],
-      [ startTimeVer + '-beta.1', 'prerelease', '-beta.2' ],
-      [ startTimeVer + '-rc.1', 'prerelease', '-rc.2' ]
+      [startTimeVer + '-alpha.1', 'prerelease', '-alpha.2'],
+      [startTimeVer + '-beta.1', 'prerelease', '-beta.2'],
+      [startTimeVer + '-rc.1', 'prerelease', '-rc.2']
     ]
     const testPretype = [
-      [ startTimeVer + '-alpha.1', 'pretype', startTimeVer + '-beta.0' ],
-      [ startTimeVer + '-beta.1', 'pretype', startTimeVer + '-rc.0' ]
+      [startTimeVer + '-alpha.1', 'pretype', startTimeVer + '-beta.0'],
+      [startTimeVer + '-beta.1', 'pretype', startTimeVer + '-rc.0']
     ]
     beforeAll(async() => {
       const start = new Date()
       preTS = ver.makeTS(start)
-      await new Promise(r => setTimeout(r, 1000))
+      await new Promise(resolve => setTimeout(resolve, 1000))
       resultReleased = testReleased
-        .map(([ currVer, increment, nextMajor ]) => [ ver.nextVersion({ currVer, increment }), nextMajor ])
+        .map(([currVer, increment, nextMajor]) => [ver.nextVersion({ currVer, increment }), nextMajor])
       resultPreInit = testPreInit
-        .map(([ currVer, increment, result]) => [ ver.nextVersion({ currVer, increment }) ])
+        .map(([currVer, increment, result]) => [ver.nextVersion({ currVer, increment })])
       resultPreOngoing = testPreOngoing
-        .map(([ currVer, increment, nextPre]) => [ ver.nextVersion({ currVer, increment }) ])
+        .map(([currVer, increment, nextPre]) => [ver.nextVersion({ currVer, increment })])
       resultPretype = testPretype
-        .map(([ currVer, increment ]) => [ ver.nextVersion({ currVer, increment })])
-      await new Promise(r => setTimeout(r, 1000))
+        .map(([currVer, increment]) => [ver.nextVersion({ currVer, increment })])
+      await new Promise(resolve => setTimeout(resolve, 1000))
       const end = new Date()
       postTS = ver.makeTS(end)
     })
-    
-    // TODO: it would be nice to do these as some kind of loop, but since we do all our calculations at once (in the 
-    //beforeAll) in order to minimize the sleep times, '.each' won't work because the values are calculated before the 
+
+    // TODO: it would be nice to do these as some kind of loop, but since we do all our calculations at once (in the
+    // beforeAll) in order to minimize the sleep times, '.each' won't work because the values are calculated before the
     // before.
     describe('released increment', () => {
       test(`${testReleased[0][0]} + ${testReleased[0][1]} -> ${testReleased[0][2]}.<newTS>`, () => {
         expect(resultReleased[0][0]).toMatch(/1\.\d{8}.\d{6}Z/)
-        const raw = [ '1.' + preTS, resultReleased[0][0], `${testReleased[0][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultReleased[0][0], `${testReleased[0][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
 
       test(`${testReleased[1][0]} + ${testReleased[1][1]} -> ${testReleased[1][2]}.<newTS>`, () => {
         expect(resultReleased[1][0]).toMatch(/1\.\d{8}.\d{6}Z/)
-        const raw = [ '1.' + preTS, resultReleased[1][0], `${testReleased[1][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultReleased[1][0], `${testReleased[1][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
 
       test(`${testReleased[2][0]} + ${testReleased[2][1]} -> ${testReleased[2][2]}.<newTS>`, () => {
         expect(resultReleased[2][0]).toMatch(/1\.\d{8}.\d{6}Z/)
-        const raw = [ '1.' + preTS, resultReleased[2][0], `${testReleased[2][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultReleased[2][0], `${testReleased[2][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
 
       test(`${testReleased[3][0]} + ${testReleased[3][1]} -> ${testReleased[3][2]}.<newTS>`, () => {
         expect(resultReleased[3][0]).toMatch(/2\.\d{8}.\d{6}Z/)
-        const raw = [ '1.' + preTS, resultReleased[3][0], `${testReleased[3][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultReleased[3][0], `${testReleased[3][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
@@ -115,36 +115,36 @@ describe('nextVersion', () => {
     describe('initial pre-increment', () => {
       test(`${testPreInit[0][0]} + ${testPreInit[0][1]} -> 1.<newTS>${testPreInit[0][3]}`, () => {
         expect(resultPreInit[0][0]).toMatch(/1\.\d{8}.\d{6}Z-alpha\.0/)
-        const raw = [ '1.' + preTS, resultPreInit[0][0], `${testPreInit[0][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultPreInit[0][0], `${testPreInit[0][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
 
       test(`${testPreInit[1][0]} + ${testPreInit[1][1]} -> ${testPreInit[1][2]}.<newTS>${testPreInit[1][3]}`, () => {
         expect(resultPreInit[1][0]).toMatch(/1\.\d{8}.\d{6}Z-alpha\.0/)
-        const raw = [ '1.' + preTS, resultPreInit[1][0], `${testPreInit[1][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultPreInit[1][0], `${testPreInit[1][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
 
       test(`${testPreInit[2][0]} + ${testPreInit[2][1]} -> ${testPreInit[2][2]}.<newTS>${testPreInit[2][3]}`, () => {
         expect(resultPreInit[2][0]).toMatch(/2\.\d{8}.\d{6}Z-alpha\.0/)
-        const raw = [ '1.' + preTS, resultPreInit[2][0], `${testPreInit[2][2]}.${postTS}` ]
+        const raw = ['1.' + preTS, resultPreInit[2][0], `${testPreInit[2][2]}.${postTS}`]
         const sorted = [...raw].sort()
         expect(raw).toEqual(sorted)
       })
     })
 
     describe('ongoing pre-increment', () => {
-      test(`${testPreOngoing[0][0]} + ${testPreOngoing[0][1]} -> ${testPreOngoing[0][0].slice(0,-1) + '2'}`, () => {
+      test(`${testPreOngoing[0][0]} + ${testPreOngoing[0][1]} -> ${testPreOngoing[0][0].slice(0, -1) + '2'}`, () => {
         expect(resultPreOngoing[0][0]).toBe(testPreOngoing[0][0].slice(0, -1) + '2')
       })
 
-      test(`${testPreOngoing[1][0]} + ${testPreOngoing[1][1]} -> ${testPreOngoing[1][0].slice(0,-1) + '2'}`, () => {
+      test(`${testPreOngoing[1][0]} + ${testPreOngoing[1][1]} -> ${testPreOngoing[1][0].slice(0, -1) + '2'}`, () => {
         expect(resultPreOngoing[1][0]).toBe(testPreOngoing[1][0].slice(0, -1) + '2')
       })
 
-      test(`${testPreOngoing[2][0]} + ${testPreOngoing[2][1]} -> ${testPreOngoing[2][0].slice(0,-1) + '2'}`, () => {
+      test(`${testPreOngoing[2][0]} + ${testPreOngoing[2][1]} -> ${testPreOngoing[2][0].slice(0, -1) + '2'}`, () => {
         expect(resultPreOngoing[2][0]).toBe(testPreOngoing[2][0].slice(0, -1) + '2')
       })
     })
@@ -158,10 +158,10 @@ describe('nextVersion', () => {
 
       test(`${startTimeVer}-rc.1 + pretype -> 1.<newTS>`, () => {
         const currVer = startTimeVer + '-rc.1'
-        const newVer = ver.nextVersion({ currVer, increment: 'pretype' })
+        const newVer = ver.nextVersion({ currVer, increment : 'pretype' })
         expect(newVer).toMatch(/^\d+.\d{8}.\d{6}Z$/)
         expect(newVer).not.toBe(startTimeVer)
-        const sorted = [ startTimeVer, newVer ].sort()
+        const sorted = [startTimeVer, newVer].sort()
         expect(sorted[0]).toBe(startTimeVer)
       })
     })
