80d398ad0bb02eea3ba6a68e520289ad7cd61058
Zane Rockenbaugh <zane@liquid-labs.com>
2025-10-12 14:32:12 -0500

    test additional X-range, hyphen range, tilde range, and caret range with bugifx in our X-range RE

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,5 +1,5 @@
 export const xRangeRE =
   //  v single tuple  v doublpe tuple             v triple tuple            v quad/pre-release tuple
   //          v if there's more than a single digit, it can't lead with a zero
-  /^(?:[x*]|[1-9]?[0-9]+\.[x*]|(?:[1-9]?[0-9]+\.){2}[x*]|(?:[1-9]?[0-9]+\.){2}[1-9]?[0-9]+-(?:alpha|beta|rc)\.[x*])$/
-export const prereleaseXRangeRE = /^(?:[1-9]?[0-9]+\.){2}[1-9]?[0-9]+-(?:alpha|beta|rc)\.[x*]$/
+  /^(?:[Xx*]|[1-9]?[0-9]+\.[Xx*]|(?:[1-9]?[0-9]+\.){2}[Xx*]|(?:[1-9]?[0-9]+\.){2}[1-9]?[0-9]+-(?:alpha|beta|rc)\.[Xx*])$/
+export const prereleaseXRangeRE = /^(?:[1-9]?[0-9]+\.){2}[1-9]?[0-9]+-(?:alpha|beta|rc)\.[Xx*]$/
--- src/min-version-string.mjs
+++ src/min-version-string.mjs
@@ -6,6 +6,7 @@ const minVersionString = (range, options) => {
     options = setDefaultOptions(options)
     // given range '1.0.0-alpha.x', semver treats that as a specific version and says the min version is
     // '1.0.0-alpha.x' even when 'options.includePrerelease' is true; this is verified in the unit tests.
+    console.log(`********\nrange: ${range}\noptions: ${JSON.stringify(options)}\n********`) // DEBUG
     if (options.includePrerelease === true && range.match(prereleaseXRangeRE)) {
         return range.slice(0, -1) + '0'
     }
